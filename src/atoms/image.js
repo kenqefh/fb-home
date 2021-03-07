c3a3f61185d95d19c0a79ef402835db3777dc0fb
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-07 18:45:10 -0500

    Add loading efect to small images

--- src/atoms/image.js
+++ src/atoms/image.js
@@ -1,8 +1,10 @@
 import React, { useLayoutEffect, useRef, useState } from "react";
 import { extractAbbreviate } from "./../utils/strings";
+import { AiOutlineLoading } from "react-icons/ai";
+import "./image.css";
 
 function ImageProfile({ profile, name }) {
-  const [loading, setLoading] = useState(true);
+  const [state, setState] = useState("loading");
   const profileImage = useRef();
 
   useLayoutEffect(() => {
@@ -10,8 +12,25 @@ function ImageProfile({ profile, name }) {
   }, []);
 
   const handleLoadProfile = function () {
-    setLoading(false);
-    profileImage.current.style.display = "block";
+    setTimeout(() => {
+      setState("success");
+      profileImage.current.style.display = "block";
+    }, 420);
+  };
+
+  const handleLoadProfileError = function () {
+    setState("error");
+  };
+
+  const renderMessage = (_) => {
+    switch (state) {
+      case "error":
+        return <span>{extractAbbreviate(name)}</span>;
+      case "loading":
+        return <AiOutlineLoading className="load_icon" />;
+      default:
+        break;
+    }
   };
 
   return (
@@ -23,8 +42,9 @@ function ImageProfile({ profile, name }) {
         alt=""
         loading="lazy"
         onLoad={handleLoadProfile}
+        onError={handleLoadProfileError}
       />
-      {loading && <span>{extractAbbreviate(name)}</span>}
+      {renderMessage()}
     </>
   );
 }
